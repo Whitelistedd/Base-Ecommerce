6227b80824ccb659b604b37a31f3d9ba4190536c
Whitelistedd <geforcexxy@gmail.com>
2022-07-16 22:13:32 +0300

    fixed order endpoint not giving payment url

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from 'axios'
-import { Model } from 'mongoose'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { connect } from '../../../lib/connection'
 import { orderProductType } from '../../../lib/models/Order'
@@ -11,23 +10,21 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== 'POST') {
-    return res.status(500).json({ message: 'not a GET request' })
+    return res.status(500).json({ message: 'not a POST request' })
   }
 
   try {
-    const { OrderSchema } = await connect()
+    const { OrderSchema, ProductSchema } = await connect()
     let newOrder = await new OrderSchema(req.body)
 
     if (newOrder === undefined) {
-      return res
-        .status(400)
-        .json({ message: 'something is wrong with your order' })
+      return res.status(400).json({ message: 'что-то не так с вашим заказом' })
     }
 
     if (newOrder.products.length < 1) {
       return res
         .status(400)
-        .json({ message: 'please add products to your cart' })
+        .json({ message: 'пожалуйста, добавьте товар или товары в корзину' })
     }
 
     /* взять все идентификаторы продуктов и количества */
@@ -43,7 +40,7 @@ export default async function handler(
       productIDS.map(async (product: { id: string; qty: number }) => {
         return {
           qty: product.qty as unknown as number,
-          product: (await OrderSchema.findOne({
+          product: (await ProductSchema.findOne({
             _id: product.id,
           })) as ProductDataType,
         }
@@ -51,12 +48,13 @@ export default async function handler(
     ).catch((err) => console.log(err))
 
     /* функция проверки наличия всех товаров на складе из базы данных */
+    console.log(confirmedProducts)
     confirmedProducts?.forEach(
       (Info) =>
         Info.product.inStock === false &&
         res.status(400).json({
           message:
-            'one of the products are not inStock, please go to your cart and delete the item',
+            'одного из товаров нет в наличии, перейдите в корзину и удалите товар',
         })
     )
 
@@ -106,6 +104,7 @@ export default async function handler(
     res.status(200).json(response.data)
     const ConfirmedOrder = await newOrder.save()
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ message: 'something went wrong' })
   }
 }
