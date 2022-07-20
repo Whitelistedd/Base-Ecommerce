e2a1b650c2abefc5c135e0a2502d74ceaeb723ad
Whitelistedd <geforcexxy@gmail.com>
2022-07-20 13:20:02 +0300

    removed useless private route component

--- src/data.ts
+++ src/data.ts
@@ -115,13 +115,8 @@ export const MenuItems = [
     path: '/products?filter=women',
   },
   {
-    id: 'nn534',
-    text: 'Наборы',
-    path: '/products?filter=sets',
-  },
-  {
     id: 'gdJZw',
-    text: 'ЧЗВ',
+    text: 'FAQ',
     path: '/FAQ',
   },
 ]
