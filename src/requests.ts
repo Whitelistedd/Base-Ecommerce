0ee12c590bc37032b9dcb5c6ad3ee8939e78208f
Whitelistedd <geforcexxy@gmail.com>
2023-04-01 04:48:39 +0300

    fixed api url again

--- src/requests.ts
+++ src/requests.ts
@@ -3,11 +3,11 @@ import axios from 'axios'
 const baseURL =
   process.env.NEXT_PUBLIC_ENVIRONMENT === 'local'
     ? process.env.NEXT_PUBLIC_APP_BASEURL
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL?.split('/')[3]}`
+    : process.env.NEXT_PUBLIC_VERCEL_URL
 
 export const BASE_URL = baseURL
 
 /* создаст запрос с URL-адресом из env */
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `https://${BASE_URL?.split('/')[3]}`,
 })
